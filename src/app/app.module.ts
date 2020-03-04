301f04b36e614da41c16c4949f661ce1274afd8f
Zoboki Árpád <h671213@stud.u-szeged.hu>
2020-03-04 23:08:18 +0100

    searchfield works
    autocomplete
    actual api fetching based on input
    deep purple material style
    
    todo: would be nice to cache results so we don't flood poor opendota

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeroCounterSearchComponent } from './hero-counter-search/hero-counter-search.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatAutocompleteModule} from '@angular/material/autocomplete';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeroCounterSearchComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule
-  ],
+  declarations: [AppComponent, HeroCounterSearchComponent],
+  imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, MatAutocompleteModule, BrowserAnimationsModule, MatFormFieldModule
+  , MatInputModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/hero-counter-search/hero-counter-search.component.ts
+++ src/app/hero-counter-search/hero-counter-search.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import * as heroesJson from "../../db/heroes.json";
+import { FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
+import {map, startWith} from 'rxjs/operators';
 
 @Component({
   selector: "app-hero-counter-search",
@@ -12,24 +15,48 @@ export class HeroCounterSearchComponent implements OnInit {
 
   private heroes: IHeroData[] = [];
 
+  searchControl = new FormControl();
+  options: string[] = heroesJson.heroes.map(h => h.localized_name).sort();
+  filteredOptions: Observable<string[]>;
+
   ngOnInit() {
+    this.filteredOptions = this.searchControl.valueChanges.pipe(startWith(''), map(value => this._filter(value)));
+    this.searchControl.valueChanges.subscribe(value => {
+      const foundHero = heroesJson.heroes.find(h => h.localized_name === value);
+      if (foundHero) {
+        this.fetchHeroCounters(foundHero.id, 5);
+      }
+    })
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter(option => option.toLowerCase().includes(filterValue));
+  }
+
+  private fetchHeroCounters(heroId: number, limit: number) {
     let sortedCounters: IHeroCounterData[];
     this.http
-      .get("https://api.opendota.com/api/heroes/1/matchups")
-      .subscribe((matchData: IHeroCounterData[]) => {
-        sortedCounters = matchData.sort(
-          (a, b) => a.wins / a.games_played - b.wins / b.games_played
-        );
-        for (let i = 0; i < 5; i++) {
-          const bestIndex = sortedCounters[i].hero_id;
-          this.heroes.push({
-            localized_name: heroesJson.heroes[bestIndex].localized_name,
-            url_large_portrait: heroesJson.heroes[bestIndex].url_large_portrait
-          });
-        }
-      });
+    .get("https://api.opendota.com/api/heroes/" + heroId + "/matchups")
+    .subscribe((matchData: IHeroCounterData[]) => {
+      sortedCounters = matchData.sort(
+        (a, b) => a.wins / a.games_played - b.wins / b.games_played
+      );
+      this.heroes = [];
+      const heroesMap = heroesJson.heroes;
+      for (let i = 0; i < limit; i++) {
+        const bestIndex = sortedCounters[i].hero_id;
+        this.heroes.push({
+          localized_name: heroesMap[bestIndex].localized_name,
+          url_large_portrait: heroesMap[bestIndex].url_large_portrait
+        });
+      }
+    });
   }
 }
+
+
 
 interface IHeroCounterData {
   games_played: number;
