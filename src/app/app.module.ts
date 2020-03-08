bcc92719fa332af7bbb612cc359a632a7a7c81e5
Zoboki Árpád <h671213@stud.u-szeged.hu>
2020-03-08 20:14:03 +0100

    formatting, % fixes
    actually the winrates were sorted the wrong way around xD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,12 @@ import {MatAutocompleteModule} from '@angular/material/autocomplete';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [AppComponent, HeroCounterSearchComponent],
   imports: [BrowserModule, HttpClientModule, ReactiveFormsModule, MatAutocompleteModule, BrowserAnimationsModule, MatFormFieldModule
-  , MatInputModule],
+  , MatInputModule, MatCardModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/hero-counter-search/hero-counter-search.component.ts
+++ src/app/hero-counter-search/hero-counter-search.component.ts
@@ -8,7 +8,7 @@ import {map, startWith} from 'rxjs/operators';
 @Component({
   selector: "app-hero-counter-search",
   templateUrl: "./hero-counter-search.component.html",
-  styleUrls: ["./hero-counter-search.component.scss"]
+  styleUrls: ["./hero-counter-search.component.scss", "./tableCSS.css"]
 })
 export class HeroCounterSearchComponent implements OnInit {
   constructor(private http: HttpClient) {}
@@ -26,6 +26,7 @@ export class HeroCounterSearchComponent implements OnInit {
         this.fetchHeroCounters(foundHero.id, 5);
       }
     })
+    this.searchControl.setValue("Axe");
   }
 
   private _filter(value: string): string[] {
@@ -37,10 +38,10 @@ export class HeroCounterSearchComponent implements OnInit {
   private fetchHeroCounters(heroId: number, limit: number) {
     let sortedCounters: IHeroCounterData[];
     this.http
-    .get("https://api.opendota.com/api/heroes/" + heroId + "/matchups")
+    .get("https://api.opendota.com/api/heroes/" + heroId + '/matchups')
     .subscribe((matchData: IHeroCounterData[]) => {
       sortedCounters = matchData.sort(
-        (a, b) => a.wins / a.games_played - b.wins / b.games_played
+        (a, b) => b.wins / b.games_played - a.wins / a.games_played
       );
       this.heroes = [];
       const heroesMap = heroesJson.heroes;
@@ -48,7 +49,8 @@ export class HeroCounterSearchComponent implements OnInit {
         const bestIndex = sortedCounters[i].hero_id;
         this.heroes.push({
           localized_name: heroesMap[bestIndex].localized_name,
-          url_large_portrait: heroesMap[bestIndex].url_large_portrait
+          url_large_portrait: heroesMap[bestIndex].url_large_portrait,
+          winrate: (sortedCounters[i].wins / sortedCounters[i].games_played * 100).toFixed(2)
         });
       }
     });
@@ -66,4 +68,5 @@ interface IHeroCounterData {
 interface IHeroData {
   localized_name: string;
   url_large_portrait: string;
+  winrate: string;
 }
